ac68baf28aaec483907bf10ed866a818b26d35f8
S Mayur <Mayurmamatha9916@gmail.com>
2025-08-31 23:59:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getbmivalue(){
     var height = document.getElementById('height').value;
 
     height=height*12;
-    height=height*0.025; //Now height is in meter
+    height=height*0.025;
 
     var newbmivalue = weight/Math.pow(height,2);
     newbmivalue=Math.round(newbmivalue);
